125ea834af256a09f970f1230a59b8344bb32a2d
BB <89454659+BekzatBagdat@users.noreply.github.com>
2023-11-18 23:49:49 +0100

    updt

---
+++ js/blogs.js
@@ -0,0 +1,6 @@
+//Removing load more btn after the click
+const loadMoreBtn = document
+  .querySelector(".load-more-btn")
+  .addEventListener("click", () => {
+    document.querySelector(".load-more-btn").style.display = "none";
+  });
