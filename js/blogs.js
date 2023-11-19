7c0541ba5640288e84d05a21d6bd292c0d640b3d
BB <89454659+BekzatBagdat@users.noreply.github.com>
2023-11-19 02:30:43 +0100

    updt

--- js/blogs.js
+++ js/blogs.js
@@ -1,6 +1,9 @@
-//Removing load more btn after the click
-const loadMoreBtn = document
-  .querySelector(".load-more-btn")
-  .addEventListener("click", () => {
-    document.querySelector(".load-more-btn").style.display = "none";
-  });
+const removeLoadMoreBtn = () => {
+  //Removing load more btn after the click
+  const loadMoreBtn = document
+    .querySelector(".load-more-btn")
+    .addEventListener("click", () => {
+      document.querySelector(".load-more-btn").style.display = "none";
+    });
+};
+removeLoadMoreBtn();
--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 // Homepage carousel (slider)
-const scrollContainer = document.querySelector(".home-recent-container");
-const backBtn = document
-  .querySelector(".back-btn")
-  .addEventListener("click", () => {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft -= 1100;
-  });
-const forwardBtn = document
-  .querySelector(".forward-btn")
-  .addEventListener("click", () => {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += 1100;
-  });
+
+const homeCarousel = () => {
+  const scrollContainer = document.querySelector(".home-recent-container");
+
+  const backBtn = document
+    .querySelector(".back-btn")
+    .addEventListener("click", () => {
+      scrollContainer.style.scrollBehavior = "smooth";
+      scrollContainer.scrollLeft -= 1100;
+    });
+
+  const forwardBtn = document
+    .querySelector(".forward-btn")
+    .addEventListener("click", () => {
+      scrollContainer.style.scrollBehavior = "smooth";
+      scrollContainer.scrollLeft += 1100;
+    });
+};
+homeCarousel();
